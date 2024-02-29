40db984eb1244bcafbd23a6078719a9aecbad179
Hyosang An <gytharj@gmail.com>
2024-02-29 23:37:11 +0900

    🌱 CEngine 클래스 추가

---
+++ My_WinAPI/CEngine.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+#include "CEngine.h"
+
+CEngine::CEngine()
+{
+}
+
+CEngine::~CEngine()
+{
+}
+
+int CEngine::init(HWND _hWnd, POINT _Resolution)
+{
+	return 0;
+}
+
+void CEngine::progress()
+{
+}
+
---
+++ My_WinAPI/CEngine.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Singleton.h"
+class CEngine :
+    public Singleton<CEngine>
+{
+private:
+    HWND    m_hMainWnd;     // 메인 윈도우 핸들
+    POINT   m_Resolution;   // 메인 윈도우 해상도
+    HDC     m_hDC;          // 메인 윈도우 DC
+
+public:
+    int     init(HWND _hWnd, POINT _Resolution);
+    void    progress();
+
+private:
+    CEngine();
+    ~CEngine();
+};
+
