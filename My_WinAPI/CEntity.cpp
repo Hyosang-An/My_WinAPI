649beb906572323c60abea96a68b73f157d9a3ea
Hyosang An <gytharj@gmail.com>
2024-03-04 23:28:32 +0900

    🌱 Entity 클래스 추가

---
+++ My_WinAPI/CEntity.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "CEntity.h"
+
+// 정적 멤버 변수 초기화
+UINT CEntity::g_NextID = 0;
+
+CEntity::CEntity()
+	: m_ID(g_NextID)
+{
+}
+
+CEntity::~CEntity()
+{
+}
---
+++ My_WinAPI/CEntity.h
@@ -0,0 +1,23 @@
+#pragma once
+class CEntity
+{
+private:
+	// 정적 멤버 선언
+	static UINT g_NextID;
+
+	const UINT	m_ID;
+	wstring		m_strName;
+
+public:
+	UINT GetID() { return m_ID; }
+	void SetName(const wstring& _name) { m_strName = _name; }
+	const wstring& GetName() { return m_strName; }
+
+public:
+	// CEntity는 추상클래스이다.
+	// virtual CEntity* Clone() = 0;
+
+public:
+	CEntity();
+	virtual ~CEntity();
+};
