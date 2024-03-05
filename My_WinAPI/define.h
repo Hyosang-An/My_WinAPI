c4b15fa94a5131c9bd5714761be2b7c03a58ee4f
Hyosang An <gytharj@gmail.com>
2024-03-05 20:26:55 +0900

    🌱 enum.h 및 define.h에 항목 추가

--- My_WinAPI/define.h
+++ My_WinAPI/define.h
@@ -1,3 +1,4 @@
 #pragma once
 
 #define USE_PEN(_DC, _PEN_TYPE) CSelectObj SelectPen(_DC, CEngine::GetInstance().GetPen(_PEN_TYPE))
+#define USE_BRUSH(_DC, _BRUSH_TYPE) CSelectObj SelectBrush(_DC, CEngine::GetInstance().GetBrush(_BRUSH_TYPE))
--- My_WinAPI/enum.h
+++ My_WinAPI/enum.h
@@ -21,3 +21,30 @@ enum class BRUSH_TYPE
 
 	END
 };
+
+enum class LEVEL_TYPE
+{
+	LOGO_START,
+	EDITOR,
+
+	STAGE_01,
+	STAGE_02,
+	STAGE_03,
+
+	BOSS_01,
+
+	END,
+};
+
+enum class PLAYER_STATE
+{
+	IDLE,
+	RUN,
+	MOVE,
+	DASH,
+	ATTACK,
+	HIT,
+	DEAD,
+
+	END,
+};
