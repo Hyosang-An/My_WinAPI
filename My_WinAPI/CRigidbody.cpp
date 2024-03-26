1ba5e71ba3a7ebe9af5053f341ee8872392a3e87
Hyosang An <gytharj@gmail.com>
2024-03-26 19:44:28 +0900

    ✨ 🎨 Rigidbody 추가 및 TopView 기준 이동 구현

--- My_WinAPI/CRigidbody.cpp
+++ My_WinAPI/CRigidbody.cpp
@@ -2,7 +2,15 @@
 #include "CRigidbody.h"
 
 CRigidbody::CRigidbody() :
-	m_fMass(0)
+	m_fMass(1),
+	m_GravityAccel(100),
+	m_Friction(10000),
+	m_MinWalkSpeed(90),
+	m_MaxWalkSpeed(200),
+	m_MaxGravitySpeed(500),
+	m_UseGravity(false),
+	m_OnGround(true),
+	m_JumpSpeed(400)
 {
 }
 
@@ -10,7 +18,49 @@ CRigidbody::~CRigidbody()
 {
 }
 
+
+
 void CRigidbody::finaltick()
 {
-}
 
+	// 중력 이외의 외력으로 인한 가속도
+	Vec2 AccelWithoutGravity = m_Force / m_fMass;
+
+	// 속도 업데이트
+	m_Velocity += AccelWithoutGravity * DT;
+
+	// 최대 속도 제한
+	if (m_MaxWalkSpeed !=0 && m_MaxWalkSpeed < m_Velocity.Length())
+		m_Velocity = m_Velocity.Normalize() * m_MaxWalkSpeed;
+
+	// 최소 속도 보정
+	if (m_MinWalkSpeed != 0 && m_Velocity.Length() < m_MinWalkSpeed && !AccelWithoutGravity.IsZero())
+	{
+		auto accel = AccelWithoutGravity;
+		m_Velocity = accel.Normalize() * m_MinWalkSpeed;
+	}
+
+	// 만약 RigidBody 에 적용된 힘이 없으면서, OnGround인데 속도는 있는경우
+	// 마찰에 의해서 현재 속도를 줄인다
+	if (m_Force.IsZero() && m_OnGround)
+	{
+		float speed = m_Velocity.Length();
+
+		speed -= m_Friction * DT;
+
+		// 마찰에 의해 속도가 음수가 될 수는 없으므로 예외처리
+		if (speed <= 0)
+			speed = 0;
+
+		if (!m_Velocity.IsZero())
+			m_Velocity.Normalize();
+
+		m_Velocity *= speed;
+	}
+
+	// 위치 업데이트
+	m_pOwner->SetPos(m_pOwner->GetPos() + m_Velocity * DT);
+
+	// 힘 초기화
+	m_Force = Vec2(0, 0);
+}
--- My_WinAPI/CRigidbody.h
+++ My_WinAPI/CRigidbody.h
@@ -6,10 +6,36 @@ class CRigidbody :
 public:
     float   m_fMass;
     Vec2    m_Velocity;
-    Vec2    m_Force;
+    Vec2    m_Force;                // 중력 이외의 외력
+
+    float   m_GravityAccel;
+
+    float   m_Friction; 
+
+    float   m_MinWalkSpeed;         // 최소 보장 걷는 속력
+    float   m_MaxWalkSpeed;         // 최대 제한 건는 속력
+    float   m_MaxGravitySpeed;      // 중력으로 인한 최대 낙하속도 제한
+
+    bool m_UseGravity;
+    bool m_OnGround;
+
+    float m_JumpSpeed;
+
+public:
+    void AddForce(Vec2 _force) { m_Force += _force; }
+    void SetMass(float _m) { m_fMass = _m; }
+
+    void SetFriction(float _friction) { m_Friction = _friction; }
+    void SetMinWalkSpeed(float _min) { m_MinWalkSpeed = _min; }
+    void SetMaxWalkSpeed(float _max) { m_MaxGravitySpeed = _max; }
+
+
+    float GetMass() { return m_fMass; }
 
 public:
     virtual void finaltick() override;
+
+
 
 public:
     CLONE(CRigidbody)
